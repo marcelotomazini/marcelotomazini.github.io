78388756b944c4bbd552febf160cc4db697e24df
marcelo.tomazini <marcelo.tomazini@objective.com.br>
2019-08-02 09:04:37 -0300

    music-finder

---
+++ music-finder/precache-manifest.43cccf73238d5f57f751bf84fd1b1601.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e983ac349edadade4c2cae0c2259d1db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07c51f0a4b655bdb53a7",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "706628725242f114fbbe",
+    "url": "/static/js/2.ff0c1bea.chunk.js"
+  },
+  {
+    "revision": "07c51f0a4b655bdb53a7",
+    "url": "/static/js/main.635ad3ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
