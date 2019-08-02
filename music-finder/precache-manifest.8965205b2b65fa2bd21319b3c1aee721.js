dbe606eeded76d9c4f272dcb41bc22acacce910c
marcelo.tomazini <marcelo.tomazini@objective.com.br>
2019-08-02 09:55:04 -0300

    music-finder

---
+++ music-finder/precache-manifest.8965205b2b65fa2bd21319b3c1aee721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572267962d1f21752dde1b220e9cdcf0",
+    "url": "/music-finder/index.html"
+  },
+  {
+    "revision": "6e55b3cda51b894c59e9",
+    "url": "/music-finder/static/css/main.d9e75851.chunk.css"
+  },
+  {
+    "revision": "706628725242f114fbbe",
+    "url": "/music-finder/static/js/2.ff0c1bea.chunk.js"
+  },
+  {
+    "revision": "6e55b3cda51b894c59e9",
+    "url": "/music-finder/static/js/main.17353b96.chunk.js"
+  },
+  {
+    "revision": "b30949b31ec626b4365c",
+    "url": "/music-finder/static/js/runtime~main.92182736.js"
+  }
+]);
--- music-finder/service-worker.js
+++ music-finder/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d52a43a238f37ca48277e040f9dafbc4.js"
+  "/music-finder/precache-manifest.8965205b2b65fa2bd21319b3c1aee721.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music-finder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
